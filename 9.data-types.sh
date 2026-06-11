9c726472866dd578955c9d5e0b420c57259d02db
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-11 21:55:55 +0530

    data-types

---
+++ 9.data-types.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#everything in shell script is a string, there are no data types like int, float, etc.
+
+
+number1=10
+number2=20
+
+sum=$((number1 + number2))
+
+echo "sum is: $sum"
