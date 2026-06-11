5d6cdfa13ce0f145b9c1071bbaab95cf786e719c
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-11 13:19:07 +0530

    conversation

---
+++ 2.conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Ram::: Hello, how are you?"
+echo "Shyam::: I am good, thank you! How about you?"
+echo "Ram::: I am doing well too. What have you been up to lately?"
+echo "Shyam::: I've been busy with work and some personal projects. How about you?"
