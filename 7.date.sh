6f304aca6a6187af00192de7a46248a2ff148493
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-11 20:58:12 +0530

    date updated

--- 7.date.sh
+++ 7.date.sh
@@ -3,7 +3,7 @@
 # DATE=$(date)
 
 start_time=$(date +%s)  # Capture the start time in seconds
-sleep 10 &
+sleep 10 # Simulating some work by sleeping for 10 seconds
 end_time=$(date +%s)  # Capture the end time in seconds
 
 total_time=$(($end_time-$start_time))
