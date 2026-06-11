58008658a75b8596cc6166b4378095938e8b9eb7
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-11 20:54:54 +0530

    date updated

--- 7.date.sh
+++ 7.date.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-DATE=$(date)
+# DATE=$(date)
 
-echo "Timestamp executed: $DATE"
+start_time=$(date +%s)  # Capture the start time in seconds
+sleep 10 &
+end_time=$(date +%s)  # Capture the end time in seconds
+
+total_time=$($end_time-$start_time)
+
+echo "Script executed in: $total_time seconds"
