98834923d2a1dce2a7d4dd2f9901e464e02e0312
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-11 20:56:55 +0530

    updated

--- 7.date.sh
+++ 7.date.sh
@@ -6,6 +6,6 @@ start_time=$(date +%s)  # Capture the start time in seconds
 sleep 10 &
 end_time=$(date +%s)  # Capture the end time in seconds
 
-total_time=$($end_time-$start_time)
+total_time=$(($end_time-$start_time))
 
 echo "Script executed in: $total_time seconds"
