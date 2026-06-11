ae0ba128f6ad6bb6080cd3d33eff00d7ea1f1523
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-11 21:23:09 +0530

    updated

---
+++ 8.special-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables passed  to the script : $@ "
+echo "All variables passed  to the script : $* "
+echo "Script name : $0 "
+echo "Current directory : $PWD "
+echo "Who is running this script : $USER "
+echo "Home directory of the user : $HOME "
+echo "Number of arguments passed to the script : $# "
+echo "First argument : $1 "
+echo "Second argument : $2 "
