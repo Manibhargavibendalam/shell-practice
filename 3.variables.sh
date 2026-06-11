cc860dbb0c5894939d5198904620f0af99505ba6
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-11 13:24:40 +0530

    variables updated

---
+++ 3.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="Ram"
+PERSON2="Shyam"
+
+echo "${PERSON1}::: Hello, how are you?"
+echo "${PERSON2}::: I am good, thank you! How about you?"
+echo "${PERSON1}::: I am doing well too. What have you been up to lately?"
+echo "${PERSON2}::: I've been busy with work and some personal projects. How about you?"
