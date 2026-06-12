cd046a3738156575c403c15b2774fbb81c70295a
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-12 12:43:05 +0530

    condtions

---
+++ 11.conditions_2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter a number:"
+read "number"
+
+if [ $((number%2)) -eq 0 ];then 
+   echo "given number $number is even"
+else 
+   echo "given number $number is odd"
+fi
