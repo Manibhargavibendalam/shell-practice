d89485e336a60b822921a18a0ca0fc5222cd80a7
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-12 22:17:38 +0530

    u

--- 12.installation.sh
+++ 12.installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
    echo "ERROR: Please run this script with root privilage"
+   exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Installing mysql is failure"
+    exit 1
 else
     echo "Installing mysql is success"
 fi
---
+++ 13.installation.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: Please run this script with root privilage"
+    exit 1
+fi
+
+dnf install mysql -y
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing mysql is failure"
+    exit 1
+else
+    echo "Installing mysql is success"
+fi
+
+
+dnf install nginx -y
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing nginx is failure"
+    exit 1  
+else
+    echo "Installing nginx is success"
+fi
