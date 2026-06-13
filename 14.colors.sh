3083f209dbf43f2ce24d408696f23f89ca11a114
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-13 10:56:39 +0530

    updated

--- 14.colors.sh
+++ 14.colors.sh
@@ -5,8 +5,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
-echo -e "$G Hello World"
+#$G is green color and $N is reset color to default..if we won't give N then it will continue with green color
+echo -e "$G Hello World" 
 echo "Check this color"
-echo -e "$R Hello World $N"
+echo -e "$R Hello World $N" #if we gibe N then it will reset the color to default
 echo "Check this color"
