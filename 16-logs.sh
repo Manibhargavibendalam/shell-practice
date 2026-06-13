90daeb5eb44cf3b776a930e62e6aaf0e0e170e7f
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-13 12:49:52 +0530

    updated

--- 16-logs.sh
+++ 16-logs.sh
@@ -13,22 +13,23 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 ) #getting script name without extension
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER #create logs folder if not exist
-echo "Script started executed at : $(date)"
+echo "Script started executed at : $(date)" | tee -a $LOG_FILE #log the start time of script execution
 
+# tee -a $LOG_FILE ::: this command will append the output of echo command to log file as well as print it on terminal. 
 
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR: Please run this script with root privilage"
+    echo "ERROR: Please run this script with root privilage" | tee -a $LOG_FILE
     exit 1
 fi
 
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-      echo -e "ERROR:: Installing $2 is... $R failure $N"
+      echo -e "ERROR:: Installing $2 is... $R failure $N" | tee -a $LOG_FILE
       exit 1
     else
-      echo -e "Installing $2 is.... $G success $N"
+      echo -e "Installing $2 is.... $G success $N" | tee -a $LOG_FILE
     fi
 }
 
@@ -37,7 +38,7 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y &>> $LOG_FILE
     VALIDATE $? "MySQL"
 else
-    echo -e "MySQL is already installed... $Y Skipping... $N"
+    echo -e "MySQL is already installed... $Y Skipping... $N" | tee -a $LOG_FILE
 fi
 
 dnf list installed nginx &>> $LOG_FILE
@@ -45,7 +46,7 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y &>> $LOG_FILE
     VALIDATE $? "Nginx"
 else
-    echo -e "Nginx is already installed... $Y Skipping... $N"
+    echo -e "Nginx is already installed... $Y Skipping... $N" | tee -a $LOG_FILE
 fi
 
 
