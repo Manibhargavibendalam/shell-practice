9a306c2ce8bd39bbd82f7a63812e9af18fd4575f
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-24 12:06:22 +0530

    monitoring

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -2,12 +2,14 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2 # in project we keep it as 75
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
 
 while IFs= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "High Usage on $PARTITION: $USAGE"
+        MESSAGE="High Usage on $PARTITION: $USAGE %"
     fi
 done <<< $DISK_USAGE
