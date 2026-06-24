933b520eed0956f3a2508453b849711987dfbdd1
Manibhargavibendalam <manibhargavibendalam@gmail.com>
2026-06-24 10:53:59 +0530

    monitoring

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep -v Filesystem )
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 # in project we keep it as 75
 
 while IFs= read -r line
 do
-   echo "line:$line"
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        echo "High Usage on $PARTITION: $USAGE"
+    fi
 done <<< $DISK_USAGE
